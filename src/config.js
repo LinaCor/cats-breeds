32d999b29fb5fdf9445baebd80bcce98e66d8226
Lina <g.korotkova@lucky-team.pro>
2024-12-02 18:05:07 +0300

    first build

---
+++ src/config.js
@@ -0,0 +1,12 @@
+const API_KEY = "live_DuWRqWLTiaUSp2kKVKTJrJQ8sie1zcqBHOgFrZhzWqqEzA4GeMCiddTmRWibLUum";
+const API_URL = "https://api.thecatapi.com/v1/images/"
+
+
+export { API_URL, API_KEY };
+
+
+//const API_URL = `https://api.thecatapi.com/v1/images/search?limit=10&has_breeds=1&api_key=${API_KEY}`;
+
+//по породе https://api.thecatapi.com/v1/images/search?limit=10&breed_ids=beng&api_key=live_DuWRqWLTiaUSp2kKVKTJrJQ8sie1zcqBHOgFrZhzWqqEzA4GeMCiddTmRWibLUum
+
+
