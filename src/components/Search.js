f906ff1e72ee9541feee40c0fa37afdb5582150f
Lina <g.korotkova@lucky-team.pro>
2024-12-04 13:21:01 +0300

    add search

---
+++ src/components/Search.js
@@ -0,0 +1,36 @@
+import { useState } from "react";
+import "../pages/my-style.css";
+
+
+export function Search({ callback }) {
+
+  const [value, setValue] = useState('');
+
+
+  const handleKey = (evt) => {
+    if (evt.key === 'Enter') {
+      handleSubmit();
+    }
+  }
+
+  const handleSubmit = () => {
+    callback(value);
+  }
+
+
+  return (
+    <div className="row">
+      <div className="input-field col s6">
+        <input
+          type="search"
+          placeholder="search breed..."
+          onKeyDown={handleKey}
+          onChange={(e) => setValue(e.target.value)}
+          value={value}
+          className="search-input"
+        />
+        <button className="btn grey darken-3 search-btn" onClick={handleSubmit}>Search</button>
+      </div>
+    </div>
+  )
+}
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,20 +1,50 @@
 import { useEffect, useState } from "react";
+import { useLocation, useNavigate } from "react-router-dom";
 import { getBreeds } from "../api";
 import { Preloader } from "../components/Preloader";
 import { CatsList } from "./CatsList.js"
+import { Search } from "../components/Search.js";
 
 export function Home() {
   const [cats, setCats] = useState([]);
+  const [pureCats, setPureCats] = useState([]);
 
+  const { search } = useLocation();
+  const navigate = useNavigate();
+
+  const handleSearch = (str) => {
+    const queryString = new URLSearchParams();
+    queryString.set('search', str);
+    navigate(`?${queryString.toString()}`, { replace: true });
+  };
 
   useEffect(() => {
-    getBreeds().then(data => setCats(data))
+    getBreeds().then(data => {
+      setCats(data);
+      setPureCats(data);
+    });
   }, []);
 
-  console.log(cats)
+  useEffect(() => {
+    if (search) {
+      const query = new URLSearchParams(search);
+      const searchTerm = query.get('search');
+
+      if (searchTerm) {
+        const filteredArr = cats.filter(item => item.breeds[0]?.name.toLowerCase().includes(searchTerm.toLowerCase()));
+        setCats(filteredArr.length ? filteredArr : pureCats);
+      } else if (searchTerm.length === 0) {
+        setCats(pureCats);
+      }
+    }
+    //eslint-disable-next-line
+  }, [search]);
+
+
 
   return (
     <>
+      <Search callback={handleSearch} />
       {cats.length ? <CatsList catalog={cats} /> : <Preloader />}
     </>
   )
